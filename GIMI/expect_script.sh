73809990d881a694a9e6e2e805c264cec2d5d6d2
UMass user <umass@dilip.(none)>
2012-07-02 20:37:45 -0400

    changes for gec14

--- GIMI/expect_script.sh
+++ GIMI/expect_script.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/expect
 set file [lindex $argv 0]
-spawn sudo cp $file.jpg /var/www
+spawn sudo cp ${file}1.jpg /var/www/html
 expect "password:"
-send "gec14user\n";
+send "\$tr3am\n";
 interact
+
+spawn sudo cp ${file}2.jpg /var/www/html
+expect "password:"
+send "\$tr3am\n";
+interact
+
