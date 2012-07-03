ebb5c5483088c405baa85a0b8a460fcaf3120fe4
UMass user <umass@dilip.(none)>
2012-07-02 20:37:12 -0400

    changes for gec14

--- GIMI/html_script.pl
+++ GIMI/html_script.pl
@@ -1,5 +1,15 @@
 #!/usr/bin/perl
 open(OUT,">oml\.html");
-print OUT "<html>\n <head>\n<meta http-equiv=\"refresh\" content=\"10\">\n</head>\n<img src=\"$ARGV[0]\.jpg\" alt=\"Iperf\"  />\n </html>";
+print OUT "<html>\n <head>\n<meta http-equiv=\"refresh\" content=\"10\">\n</head>\n
+<div style=\"float:left;\">\n
+<img src=\"$ARGV[0]1.jpg\" alt=\"Iperf Source1\"/>\n
+</div>\n
+
+<div style=\"float:right;\">\n
+<img src=\"$ARGV[0]2.jpg\" alt=\"Iperf Source2\"  />\n
+</div>\n
+
+
+</html>";
 close OUT;
 
