1f379c4c8a07869a875721c5781ba9c4555b65d7
UMass user <umass@dilip.(none)>
2012-07-02 20:38:12 -0400

    chnages for gec14

--- GIMI/tutorial_viz.sh
+++ GIMI/tutorial_viz.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 perl R_script.pl $1
 perl html_script.pl $1
-expect -c 'spawn sudo cp oml.html /var/www; expect password; send "gec14user\n"; interact'
+expect -c 'spawn sudo cp oml.html /var/www/html; expect password; send "\$tr3am\n"; interact'
 while true
 do
  rm -f $1.sq3
  iget -f $1.sq3
  R -f R_script_viz.r
- convert -density 100 $1.pdf $1.jpg
+ convert -density 100 ${1}1.pdf ${1}1.jpg
+ convert -density 100 ${1}2.pdf ${1}2.jpg
  ./expect_script.sh $1
  # expect -c 'spawn sudo cp ${NAME}.jpg /var/www/html; expect password; send "\$tr3am\n"; interact'
  sleep 15
