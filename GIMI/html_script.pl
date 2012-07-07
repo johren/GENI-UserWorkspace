9c549dae0c11b8542b4ceb786215ed37858a09ee
UMass user <umass@dilip.(none)>
2012-07-07 10:21:30 -0400

    gec14 changes

--- GIMI/html_script.pl
+++ GIMI/html_script.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 open(OUT,">oml\.html");
 print OUT "<html>\n <head>\n<meta http-equiv=\"refresh\" content=\"10\">\n</head>\n
-<div style=\"float:left;\">\n
+<div style=\"float:center;\">\n
 <img src=\"$ARGV[0].png\" alt=\"Iperf Source1\"/>\n
 </div>\n
 </html>";
