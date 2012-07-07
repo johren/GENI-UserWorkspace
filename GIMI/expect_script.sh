6308f5f7f106ed482e3a91a534f3bdd115439de5
UMass user <umass@dilip.(none)>
2012-07-07 09:46:39 -0400

    small changes

--- GIMI/expect_script.sh
+++ GIMI/expect_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 set file [lindex $argv 0]
-spawn sudo cp ${file}.png /var/www/html
+spawn sudo cp ${file}.png /var/www/
 expect "password:"
-send "\$tr3am\n";
+send "gec14user\n";
 interact
