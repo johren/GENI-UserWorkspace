d2dc297d6f08b9412f840bf5b77b3a2f4fe1ca87
Dilip Kumar <dilip@cs.umass.edu>
2012-07-09 09:29:01 -0400

    changes to script

--- GIMI/html_script.pl
+++ GIMI/html_script.pl
@@ -2,7 +2,7 @@
 open(OUT,">oml\.html");
 print OUT "<html>\n <head>\n<meta http-equiv=\"refresh\" content=\"10\">\n</head>\n
 <div style=\"float:center;\">\n
-<img src=\"$ARGV[0].png\" alt=\"Iperf Source1\"/>\n
+<img src=\"$ARGV[0].png\" alt=\"Experiment\"/>\n
 </div>\n
 </html>";
 close OUT;
