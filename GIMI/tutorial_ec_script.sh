1082f51228e75b8ee98c253552e85c2b16185510
Dilip Kumar <dilip@cs.umass.edu>
2012-07-08 21:48:37 -0400

    changes to script

--- GIMI/tutorial_ec_script.sh
+++ GIMI/tutorial_ec_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true
 do
-  omf-5.4 exec --no-cmc tcp_iperf.rb -- --source1 nodeA --sink nodeC
+  omf-5.4 exec --no-cmc -S gimiXX-tutorial tcp_iperf.rb -- --source1 gimiXX-tutorial-nodeA --sink gimiXX-tutorial-nodeC
   sleep 5
 done
